fdc7e4121b321d9537ff1d5647ec63e66c515a91
cd-jung <akchuimy@gmail.com>
2023-09-03 10:11:53 +0800

    refactoring folder structure

--- apps/analyser/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-class DeviceStateAnalyserTestCase(TestCase):
-    def setUp(self):
-        pass
-
-    def test_analise_a_line(self):
-        pass
