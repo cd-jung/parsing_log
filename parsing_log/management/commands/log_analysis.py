fc65a55a2cbf4af709641ae502d9b89d8102bca7
cd-jung <akchuimy@gmail.com>
2023-09-03 11:08:56 +0800

    add log analysis command tests

--- parsing_log/management/commands/log_analysis.py
+++ parsing_log/management/commands/log_analysis.py
@@ -13,11 +13,12 @@ class Command(BaseCommand):
         file_path = options['file_path']
         if not os.path.exists(file_path):
             self.stdout.write(self.style.ERROR(f"This file path{file_path} does not exist."))
-            return
+            raise FileNotFoundError()
         self.stdout.write(self.style.NOTICE(f"Starts to analyse a log file '{file_path}'."))
-        
+
         try:
             excute_analysis(file_path)
         except Exception as exc:
             self.stdout.write(self.style.ERROR(f"An eception occured while analysing data."))
             self.stdout.write(self.style.ERROR(exc))
+            raise Exception(exc)
--- parsing_log/test.py
+++ parsing_log/test.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from parsing_log.management.commands.log_analysis import Command
+import os
 
 # Create your tests here.
 class DeviceStateAnalyserTestCase(TestCase):
@@ -6,4 +8,18 @@ class DeviceStateAnalyserTestCase(TestCase):
         pass
 
     def test_analise_a_line(self):
-        pass
+        self.assertTrue(True)
+
+class LogAnalysisCommandTestCase(TestCase):
+    def setUp(self):
+        self.command = Command()
+
+    def test_log_analysis_command_success(self):
+        try:
+            self.command.handle(file_path=os.path.join(os.getcwd(), 'parsing_log', 'test_data', 'test.log'))
+        except Exception as e:
+            self.fail("test_log_analysis_command failed.")
+        
+    def test_log_analysis_command_failure_not_exists(self):
+        with self.assertRaises(FileNotFoundError):
+            self.command.handle(file_path=os.path.join(os.getcwd(), 'parsing_log', 'test_data', 'test.log2'))
