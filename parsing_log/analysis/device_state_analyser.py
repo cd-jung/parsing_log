a1f4270c5d2cafdd9549b21eee9d1d8737f9d5e0
cd-jung <akchuimy@gmail.com>
2023-09-03 16:49:11 +0800

    impletment view

--- parsing_log/analysis/device_state_analyser.py
+++ parsing_log/analysis/device_state_analyser.py
@@ -20,30 +20,38 @@ class DeviceStateAnalyser(AbstractedDataAnalyser):
     device_onoff_records = [] # {'start_time':timestamp, 'end_time':timestamp, 'duration':mill second}
     device_error_records = [] # timestamp
     total_of_on_time = 0 # mill second
+    time_records = [] # timestamp
+    start_time = 0
+    end_time = 0
 
     def __init__(self):
         pass
 
     def read_a_line(self, line: str):
         """ Parsing and store a line data. """
-        time = self._extract_time(line).timestamp()
+        time = int(self._extract_time(line).timestamp() * 1000)
         state = self._extract_state(line)
         # print(f"time:{time}, state:{state}")
+
+        self.time_records.append(time)
 
         if state == DeviceState.ERR:
             self.device_error_records.append(time)
         elif state == DeviceState.ON:
-            self.device_onoff_records.append({'start_time':time})
+            self.device_onoff_records.append({"start_time":time})
         elif state == DeviceState.OFF:
             record = self.device_onoff_records[-1]
-            record['end_time'] = time
-            record['duration'] = record['end_time'] - record['start_time']
+            record["end_time"] = time
+            record["duration"] = record["end_time"] - record["start_time"]
 
     def analyse(self):
         """ Analysing the extracted data. """
         for record in self.device_onoff_records:
             if 'duration' in record:
-                self.total_of_on_time += record['duration']
+                self.total_of_on_time += record["duration"]
+        self.time_records.sort()
+        self.start_time = self.time_records[0]
+        self.end_time = self.time_records[-1]
         
         # print('total_of_on_time :', str(self.total_of_on_time))
         # print('device_onoff_records :', str(self.device_onoff_records))
@@ -55,9 +63,11 @@ class DeviceStateAnalyser(AbstractedDataAnalyser):
         """
         result_file_path = os.path.join(os.getcwd(), 'parsing_log', 'test_data', 'test.result')
         
-        analysis = {'total_of_on_time':str(self.total_of_on_time),
-                    'device_onoff_records':str(self.device_onoff_records),
-                    'device_error_records':str(self.device_error_records)}
+        analysis = {"total_of_on_time":self.total_of_on_time,
+                    "start_time": self.start_time,
+                    "end_time": self.end_time,
+                    "device_onoff_records":self.device_onoff_records,
+                    "device_error_records":self.device_error_records}
         analysis = str(analysis)
         with open(result_file_path, 'w') as file:
             file.write(analysis)
@@ -76,7 +86,9 @@ class DeviceStateAnalyser(AbstractedDataAnalyser):
     def _convert_time(self, time_str):
         date_format = "%b %d %H:%M:%S:%f"
         try:
-            return datetime.strptime(time_str, date_format)
+            time = datetime.strptime(time_str, date_format)
+            time = time.replace(year=2023)
+            return time
         except Exception as e:
             raise ValueError("Format of data is wrong.", e)
         
--- parsing_log/views.py
+++ parsing_log/views.py
@@ -3,6 +3,8 @@
 from django import shortcuts
 from django.views.generic import base
 from django.http import HttpRequest, HttpResponse
+from datetime import datetime
+import os
 import json
 
 class HomePage(base.TemplateView):
@@ -13,7 +15,15 @@ class HomePage(base.TemplateView):
 
     def get(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
         context = {}
-        context['sum'] = 5000
-        context['analysis'] = json.dumps({'test':'test'})
+        analysis_str = self._read_result()
+        analysis = json.loads(analysis_str.replace("'", '"'))
+        context['errors'] = [ datetime.fromtimestamp(record/1000).strftime("%b %d %H:%M:%S:%f") for record in analysis['device_error_records']]
+        context['analysis'] = analysis_str
 
         return shortcuts.render(request, self.template_name, context)
+    
+    def _read_result(self):
+        result_file_path = os.path.join(os.getcwd(), 'parsing_log', 'test_data', 'test.result')
+        with open(result_file_path, 'r') as file:
+            analysis_str = file.read()
+        return analysis_str
