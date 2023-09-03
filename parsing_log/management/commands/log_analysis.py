a4834ac83ce718813b7a8f69d9450e4872959fb2
cd-jung <akchuimy@gmail.com>
2023-09-03 10:22:14 +0800

    add file existence check logic and test file.

--- parsing_log/management/commands/log_analysis.py
+++ parsing_log/management/commands/log_analysis.py
@@ -1,3 +1,4 @@
+import os
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
@@ -5,8 +6,11 @@ class Command(BaseCommand):
     help = 'Command for analysing a given log file'
 
     def add_arguments(self, parser):
-        parser.add_argument('file_name', type=str, help='File name that is analised.')
+        parser.add_argument('file_path', type=str, help='File path that is analised.')
 
     def handle(self, *args, **options):
-        file_name = options['file_name']
-        self.stdout.write(self.style.NOTICE(f"Starts to analyse a log file '{file_name}'."))
+        file_path = options['file_path']
+        if not os.path.exists(file_path):
+            self.stdout.write(self.style.ERROR(f"This file path{file_path} does not exist."))
+            return
+        self.stdout.write(self.style.NOTICE(f"Starts to analyse a log file '{file_path}'."))
