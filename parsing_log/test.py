d2713f46270795132f0dba42586e24ffa4be6419
cd-jung <akchuimy@gmail.com>
2023-09-03 10:12:24 +0800

    add test file

---
+++ parsing_log/test.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+# Create your tests here.
+class DeviceStateAnalyserTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_analise_a_line(self):
+        pass
